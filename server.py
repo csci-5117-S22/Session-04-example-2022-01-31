343ab14846d76f1c55bc1c1182d26e6e4564007a
Daniel Kluver <kluve018@umn.edu>
2022-02-02 11:53:20 -0600

    prep for heroku

--- server.py
+++ server.py
@@ -5,3 +5,18 @@ app = Flask(__name__)
 def hello_world():
     user_name = request.args.get("userName", "unknown")
     return render_template('main.html', user=user_name)
+
+guests = ["Daniel"]
+@app.route("/guest_list", methods=["GET"])
+def guest_get():
+    global guests
+    return render_template("guests.html", guests=guests)
+
+@app.route("/guest_list", methods=["POST"])
+def guest_post():
+    global guests
+
+    guest = request.form.get('guest', 'nobody')
+    guests.append(guest)
+
+    return render_template("guests.html", guests=guests)
